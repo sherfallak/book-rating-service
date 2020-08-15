5ad8a9aea908bb51c75a6b614a97b339be3c0416
sherfallak <sh.fallak@gmail.com>
2020-08-15 18:18:32 +0200

    initial commit

---
+++ src/main/java/fallak/microservices/bookratingservice/BookRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package fallak.microservices.bookratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRatingServiceApplication.class, args);
+	}
+
+}
